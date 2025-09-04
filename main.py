abfcab78fb45d7c2cf37262b14d38b4fae5e5395
AbdugofurKomilobv <komilovabdugofur635@gmail.com>
2025-09-04 19:31:09 +0500

    first commit

--- main.py
+++ main.py
@@ -65,3 +65,34 @@ ins = int(input('son: '))
 ins2 = int(input('son2: '))
 
 print(ins+ins2)
+
+
+ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+
+ins = int(input('son: '))
+ins2 = int(input('son2: '))
+
+print(ins+ins2)ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+
+ins = int(input('son: '))
+ins2 = int(input('son2: '))
+
+print(ins+ins2)
