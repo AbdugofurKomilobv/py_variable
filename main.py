b78df32378739a2a25b8a7adce11513996060a4e
AbdugofurKomilobv <komilovabdugofur635@gmail.com>
2025-09-05 18:38:33 +0500

    first commit

--- main.py
+++ main.py
@@ -344,3 +344,119 @@ ins = int(input('son: '))
 ins2 = int(input('son2: '))
 
 print(ins+ins2)
+
+ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+
+ins = int(input('son: '))
+ins2 = int(input('son2: '))
+
+print(ins+ins2)
+
+ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+
+ins = int(input('son: '))
+ins2 = int(input('son2: '))
+
+print(ins+ins2)
+
+
+
+ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+
+ins = int(input('son: '))
+ins2 = int(input('son2: '))
+
+print(ins+ins2)
+
+
+
+ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+
+ins = int(input('son: '))
+ins2 = int(input('son2: '))
+
+print(ins+ins2)
+
+
+ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+
+ins = int(input('son: '))
+ins2 = int(input('son2: '))
+
+print(ins+ins2)ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+
+ins = int(input('son: '))
+ins2 = int(input('son2: '))
+
+print(ins+ins2)
+
+
+ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+
+ins = int(input('son: '))
+ins2 = int(input('son2: '))
+
+print(ins+ins2)
