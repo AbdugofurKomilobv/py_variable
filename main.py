aa1fa857176fc80034fd17f3878588a02c1b1265
AbdugofurKomilobv <komilovabdugofur635@gmail.com>
2025-09-11 19:06:57 +0500

    first commit

--- main.py
+++ main.py
@@ -2319,3 +2319,468 @@ ins = int(input('son: '))
 ins2 = int(input('son2: '))
 
 print(ins+ins2)
+
+ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+
+ins = int(input('son: '))
+ins2 = int(input('son2: '))
+
+print(ins+ins2)
+
+ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+
+ins = int(input('son: '))
+ins2 = int(input('son2: '))
+
+print(ins+ins2)
+
+
+
+ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+
+ins = int(input('son: '))
+ins2 = int(input('son2: '))
+
+print(ins+ins2)
+
+
+
+ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+
+ins = int(input('son: '))
+ins2 = int(input('son2: '))
+
+print(ins+ins2)
+
+
+ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+
+ins = int(input('son: '))
+ins2 = int(input('son2: '))
+
+print(ins+ins2)
+yosh = 23
+
+print(ism)
+print(yosh)
+ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+
+ins = int(input('son: '))
+ins2 = int(input('son2: '))
+
+print(ins+ins2)
+
+
+ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+
+ins = int(input('son: '))
+ins2 = int(input('son2: '))
+
+print(ins+ins2)
+
+ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+
+ins = int(input('son: '))
+ins2 = int(input('son2: '))
+
+print(ins+ins2)
+
+ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+
+ins = int(input('son: '))
+ins2 = int(input('son2: '))
+
+print(ins+ins2)
+
+
+
+ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+
+ins = int(input('son: '))
+ins2 = int(input('son2: '))
+
+print(ins+ins2)
+
+
+
+ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+
+ins = int(input('son: '))
+ins2 = int(input('son2: '))
+
+print(ins+ins2)
+
+
+ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+
+ins = int(input('son: '))
+ins2 = int(input('son2: '))
+
+print(ins+ins2)
+yosh = 23
+
+print(ism)
+print(yosh)
+ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+
+ins = int(input('son: '))
+ins2 = int(input('son2: '))
+
+print(ins+ins2)
+
+
+ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+
+ins = int(input('son: '))
+ins2 = int(input('son2: '))
+
+print(ins+ins2)
+
+ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+
+ins = int(input('son: '))
+ins2 = int(input('son2: '))
+
+print(ins+ins2)
+
+ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+
+ins = int(input('son: '))
+ins2 = int(input('son2: '))
+
+print(ins+ins2)
+
+
+
+ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+
+ins = int(input('son: '))
+ins2 = int(input('son2: '))
+
+print(ins+ins2)
+
+
+
+ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+
+ins = int(input('son: '))
+ins2 = int(input('son2: '))
+
+print(ins+ins2)
+
+
+ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+
+ins = int(input('son: '))
+ins2 = int(input('son2: '))
+
+print(ins+ins2)
+yosh = 23
+
+print(ism)
+print(yosh)
+ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+
+ins = int(input('son: '))
+ins2 = int(input('son2: '))
+
+print(ins+ins2)
+
+
+ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+
+ins = int(input('son: '))
+ins2 = int(input('son2: '))
+
+print(ins+ins2)
+
+
+ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+
+ins = int(input('son: '))
+ins2 = int(input('son2: '))
+
+print(ins+ins2)
+
+ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+
+ins = int(input('son: '))
+ins2 = int(input('son2: '))
+
+print(ins+ins2)
+
+
+
+ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+
+ins = int(input('son: '))
+ins2 = int(input('son2: '))
+
+print(ins+ins2)
+
+
+
+ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+
+ins = int(input('son: '))
+ins2 = int(input('son2: '))
+
+print(ins+ins2)
+
+
+ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+
+ins = int(input('son: '))
+ins2 = int(input('son2: '))
+
+print(ins+ins2)
+yosh = 23
+
+print(ism)
+print(yosh)
+ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+
+ins = int(input('son: '))
+ins2 = int(input('son2: '))
+
+print(ins+ins2)
+
+
+ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+
+ins = int(input('son: '))
+ins2 = int(input('son2: '))
+
+print(ins+ins2)
