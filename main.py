b20408dd12ff6da2ff3fc770ca594b3b3f5df76e
AbdugofurKomilobv <komilovabdugofur635@gmail.com>
2025-09-13 19:52:48 +0500

    gg

--- main.py
+++ main.py
@@ -6531,3 +6531,26 @@ ins = int(input('son: '))
 ins2 = int(input('son2: '))
 
 print(ins+ins2)
+
+
+ins = int(input('son: '))
+ins2 = int(input('son2: '))
+
+print(ins+ins2)
+
+
+ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+ism = 'Ali'
+yosh = 23
+
+print(ism)
+print(yosh)
+
+ins = int(input('son: '))
+ins2 = int(input('son2: '))
+
+print(ins+ins2)
